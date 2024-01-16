6e6a1bd7bf6c41aa4791e923e096501318619d4f
Ryan Waldrop <142631948+Ryanalyst@users.noreply.github.com>
2024-01-15 20:02:09 -0600

    NashvilleHousing Data Cleaning.sql

--- NashvilleHousing Data Cleaning.sql
+++ NashvilleHousing Data Cleaning.sql
@@ -20,6 +20,7 @@ ALTER COLUMN SaleDate DATE;
 SELECT *
 FROM Nashville_Housing;
 
+------------------------------------------------------------------------------------------
 /*Looking through the data, there are quite a few nulls in the 
 property address column. The Unique ID appears to be a primary key for
 this table, but the Parcel ID seems like it could be connected to each 
@@ -62,7 +63,7 @@ JOIN Nashville_Housing b
 	ON a.ParcelID = b.ParcelID
 	AND a.[UniqueID ] <> b.[UniqueID ]
 WHERE a.PropertyAddress IS NULL;
-
+------------------------------------------------------------------------------------------
 /*Using our earlier query, we will now see that there are zero columns
 with a null value in the property address column.*/
 
@@ -114,7 +115,7 @@ SET PropertySplitCity =
 
 SELECT PropertyAddress, PropertySplitAddress, PropertySplitCity
 FROM Nashville_Housing;
-
+------------------------------------------------------------------------------------------
 /*There is another address column in this dataset called 
 Owner address. We'll be doing the same thing for this column
 in that we'll be breaking it into separate components. However,
@@ -162,196 +163,93 @@ SET OwnerSplitState =
 
 SELECT OwnerAddress, OwnerSplitAddress, OwnerSplitCity, OwnerSplitState
 FROM Nashville_Housing;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*CLEANING DATA IN SQL QUERIES*/
-
-Select *
-From NashvilleHousing
 ------------------------------------------------------------------------------------------
+/*The next section that we need to adjust is the Sold as vacant column.
+Upon inspecting it, we can see that the yes's and no's are also sometimes
+represented by a y or n. For uniformity's sake, we'll be changing all of the 
+y's to Yes and all of the n's to No.*/
 
---Standardize Data Format
+SELECT DISTINCT SoldAsVacant, COUNT(SoldAsVacant)
+FROM Nashville_Housing
+GROUP BY SoldAsVacant
+ORDER BY 2;
 
-Select *
-From NashvilleHousing
+/*We can go about this by using a case statement!*/
 
-ALTER TABLE NashvilleHousing
-ADD SaleDateConverted Date;
+SELECT SoldAsVacant,
+	CASE WHEN SoldAsVacant = 'Y' THEN 'Yes'
+	     WHEN SoldAsVacant = 'N' THEN 'No'
+	     ELSE SoldAsVacant 
+	END
+FROM Nashville_Housing;
 
-Update NashvilleHousing 
-SET SaleDateConverted = CONVERT(Date, SaleDate)
+/*To update the original column, I include the entire case
+statement in the update and that's all there is to it.*/
 
-Select SaleDateConverted, CONVERT(Date, SaleDate)
-From NashvilleHousing
-
--------------------------------------------------------------------------------------------
--- Populate Property Address data
-
-Select *
-From NashvilleHousing
-Order by ParcelID
-
-Select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress,
-ISNULL(a.PropertyAddress, b.PropertyAddress)
-
-/*The isnull will search for a null value in a.PropAdd and replace it
-with the data from b.PropAdd*/
-
-From NashvilleHousing a
-JOIN NashvilleHousing b on a.ParcelID = b.ParcelID
-and a.[UniqueID ] <> b.[UniqueID ]
-where a.PropertyAddress is null
-
-UPDATE a
-SET PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
-From NashvilleHousing a
-JOIN NashvilleHousing b on a.ParcelID = b.ParcelID
-and a.[UniqueID ] <> b.[UniqueID ]
-where a.PropertyAddress is null
-
---------------------------------------------------------------------------------------
-
--- Breaking out Address into individual columns (Address, City, State)
-
-Select PropertyAddress
-From NashvilleHousing
-
-Select
-SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress)-1) AS Address
-From NashvilleHousing
-
-
-Select
-SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress)-1) AS Address,
-SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress)+1, 
-LEN(PropertyAddress)) AS City
-From NashvilleHousing
-
----------------------------------------------------------------------------------------
-
-ALTER TABLE NashvilleHousing
-ADD PropertySplitAddress nvarchar(255);
-
-Update NashvilleHousing 
-SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress)-1)
-
-
-ALTER TABLE NashvilleHousing
-ADD PropertySplitCity nvarchar(255);
-
-Update NashvilleHousing 
-SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress)+1, 
-LEN(PropertyAddress))
-
-Select *
-From NashvilleHousing
-
-
----------------------------------------------------------------------------------------
-/*Splitting owner address into address, city, state via Parsename.*/
-
-Select OwnerAddress
-From NashvilleHousing
-
-Select PARSENAME(Replace(OwnerAddress,',', '.'), 3) as Address
-, PARSENAME(Replace(OwnerAddress,',', '.'), 2) as City
-, PARSENAME(Replace(OwnerAddress,',', '.'), 1) as State
-From NashvilleHousing
-
-
-ALTER TABLE NashvilleHousing
-ADD OwnerSplitAddress nvarchar(255);
-
-Update NashvilleHousing 
-SET OwnerSplitAddress = PARSENAME(Replace(OwnerAddress,',', '.'), 3)
-
-ALTER TABLE NashvilleHousing
-ADD OwnerSplitCity nvarchar(255);
-
-Update NashvilleHousing 
-SET OwnerSplitCity = PARSENAME(Replace(OwnerAddress,',', '.'), 2)
-
-ALTER TABLE NashvilleHousing
-ADD OwnerSplitState nvarchar(255);
-
-Update NashvilleHousing 
-SET OwnerSplitState = PARSENAME(Replace(OwnerAddress,',', '.'), 1)
-
----------------------------------------------------------------------------------------
-
---Change Y and N to Yes and No in "Sold as Vacant" field
-
-Select Distinct(SoldAsVacant), Count(SoldAsVacant)
-From NashvilleHousing
-Group by SoldAsVacant
-Order by 2
-
-
-Select SoldAsVacant,
-	CASE when SoldAsVacant = 'Y' THEN 'Yes'
-		 when SoldAsVacant = 'N' THEN 'No'
-		 ELSE SoldAsVacant
-		 END
-From NashvilleHousing
-
-Update NashvilleHousing SET
-SoldAsVacant = CASE when SoldAsVacant = 'Y' THEN 'Yes'
-		 when SoldAsVacant = 'N' THEN 'No'
-		 ELSE SoldAsVacant
-		 END
+UPDATE Nashville_Housing SET
+SoldAsVacant = CASE WHEN SoldAsVacant = 'Y' THEN 'Yes'
+		 WHEN SoldAsVacant = 'N' THEN 'No'
+		 ELSE SoldAsVacant 
+		 END;
 ------------------------------------------------------------------------------------------
+/*Upon checking the sold as vacant column, we can now see that
+the answers are all Yes or No.*/
 
--- Remove Duplicates
+SELECT DISTINCT(SoldAsVacant)
+FROM Nashville_Housing;
 
-Select *,
-	ROW_NUMBER() OVER (
-	PARTITION BY ParcelID,
-				 PropertyAddress,
-				 SalePrice,
-				 SaleDate,
-				 LegalReference
-				 ORDER BY 
-					UniqueId) as row_num
-
-From NashvilleHousing
-Order by row_num desc
+/*This last section will be about removing our duplicates
+by using window functions and then throwing that into a cte
+so that we can further filter it based on whether there are multiple
+rows of the same data. We are partitioning by multiple columns 
+that realistically wouldn't be shared with two different rows.
+We need to partition the data this way because every row has a
+uniqueID that is unique to it, despite it being a duplicate row.*/
 
 WITH RowNumCTE AS (
-Select *,
-	ROW_NUMBER() OVER (
-	PARTITION BY ParcelID,
+SELECT *,
+	ROW_NUMBER() OVER(
+		PARTITION BY ParcelID,
 				 PropertyAddress,
 				 SalePrice,
 				 SaleDate,
 				 LegalReference
-				 ORDER BY 
-					UniqueId) as row_num
+				 ORDER BY UniqueID
+				 ) row_num
+FROM Nashville_Housing)
 
-From NashvilleHousing
---Order by ParcelID
-)
+SELECT *
+FROM RowNumCTE
+WHERE row_num > 1;
 
-Select *
-From RowNumCTE
-Where row_num > 1;
+/*Now we need to delete these duplicate rows. To do this, we simply replace
+the select * statement with Delete and then rerun the code.*/
+
+WITH RowNumCTE AS (
+SELECT *,
+	ROW_NUMBER() OVER(
+		PARTITION BY ParcelID,
+				 PropertyAddress,
+				 SalePrice,
+				 SaleDate,
+				 LegalReference
+				 ORDER BY UniqueID
+				 ) row_num
+FROM Nashville_Housing)
+
+DELETE
+FROM RowNumCTE
+WHERE row_num > 1;
+
+/*Now we just rerun the previous code with the SELECT * statement and we'll find that
+there are no more duplicates.*/
+
+
+------------------------------------------------------------------------------------------
+/*Removing our unneeded columns*/
 
 ALTER TABLE NashvilleHousing
-DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress
-
-ALTER TABLE NashvilleHousing
-DROP COLUMN SaleDate
+DROP COLUMN OwnerAddress, PropertyAddress;
 
 --It's not good practice to delete RAW data, this is typically done with views and temp tables
 
